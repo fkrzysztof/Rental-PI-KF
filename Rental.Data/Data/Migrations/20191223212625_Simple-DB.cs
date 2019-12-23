5f3ae6f5f8d79054fc3ed5e4c80fa7bd5725873a
fkrzysztof <krzysztofranczyk@gmail.com>
2019-12-23 22:29:58 +0100

    DB-simple-migration

---
+++ Rental.Data/Data/Migrations/20191223212625_Simple-DB.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rental.Data
+{
+    public partial class SimpleDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AirConditionings",
+                columns: table => new
+                {
+                    AirConditioningID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AirConditionings", x => x.AirConditioningID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    BrandID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.BrandID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colours",
+                columns: table => new
+                {
+                    ColourID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colours", x => x.ColourID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrentPrices",
+                columns: table => new
+                {
+                    CurrentPriceID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleID = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: true),
+                    DateTimeFrom = table.Column<DateTime>(nullable: true),
+                    DateTimeTo = table.Column<DateTime>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrentPrices", x => x.CurrentPriceID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EngineTypes",
+                columns: table => new
+                {
+                    EngineTypeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineTypes", x => x.EngineTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipment",
+                columns: table => new
+                {
+                    EquipmentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ABS = table.Column<bool>(nullable: true),
+                    AUX = table.Column<bool>(nullable: true),
+                    ISOFIX = table.Column<bool>(nullable: true),
+                    CD = table.Column<bool>(nullable: true),
+                    SD = table.Column<bool>(nullable: true),
+                    USB = table.Column<bool>(nullable: true),
+                    CruiseControl = table.Column<bool>(nullable: true),
+                    Navigation = table.Column<bool>(nullable: true),
+                    Airbag = table.Column<bool>(nullable: true),
+                    PowerSteering = table.Column<bool>(nullable: true),
+                    AirConditioningID = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipment", x => x.EquipmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExactTypes",
+                columns: table => new
+                {
+                    ExactTypeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GeneralTypeID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExactTypes", x => x.ExactTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GearBoxes",
+                columns: table => new
+                {
+                    GearBoxID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GearBoxes", x => x.GearBoxID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GeneralTypes",
+                columns: table => new
+                {
+                    GeneralTypeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralTypes", x => x.GeneralTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    NewsID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LinkName = table.Column<string>(maxLength: 20, nullable: false),
+                    Title = table.Column<string>(maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Position = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.NewsID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    PageID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LinkName = table.Column<string>(maxLength: 10, nullable: false),
+                    Title = table.Column<string>(maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(MAX)", nullable: true),
+                    Position = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.PageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    PictureID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleID = table.Column<int>(nullable: false),
+                    URL = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.PictureID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentalAgencies",
+                columns: table => new
+                {
+                    RentalAgencyID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactPerson = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Phone1 = table.Column<string>(nullable: true),
+                    Phone2 = table.Column<string>(nullable: true),
+                    Email1 = table.Column<string>(nullable: true),
+                    Email2 = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: true),
+                    Annotation = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalAgencies", x => x.RentalAgencyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentalAgencyAddresses",
+                columns: table => new
+                {
+                    RentalAgencyAddressID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: true),
+                    ZIPCode = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true),
+                    RentalAgencyID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalAgencyAddresses", x => x.RentalAgencyAddressID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleModels",
+                columns: table => new
+                {
+                    VehicleModelID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleModels", x => x.VehicleModelID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    VehicleID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandID = table.Column<int>(nullable: false),
+                    VehicleModelID = table.Column<int>(nullable: false),
+                    YearOfProduction = table.Column<DateTime>(nullable: true),
+                    RentalAgencyID = table.Column<int>(nullable: false),
+                    EngineCapacity = table.Column<int>(nullable: true),
+                    EquipmentID = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    GeneralTypeID = table.Column<int>(nullable: false),
+                    ExactTypeID = table.Column<int>(nullable: false),
+                    EngineTypeID = table.Column<int>(nullable: false),
+                    Mileage = table.Column<string>(nullable: true),
+                    ColourID = table.Column<int>(nullable: false),
+                    VIN = table.Column<string>(nullable: true),
+                    DateIn = table.Column<DateTime>(nullable: true),
+                    DateOut = table.Column<DateTime>(nullable: true),
+                    NumberPlate = table.Column<string>(nullable: true),
+                    EnginePower = table.Column<int>(nullable: true),
+                    GearBoxID = table.Column<int>(nullable: false),
+                    WheelDriveID = table.Column<int>(nullable: false),
+                    NumberOfSeats = table.Column<byte>(nullable: true),
+                    NumberOfDoors = table.Column<byte>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.VehicleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WheelDrives",
+                columns: table => new
+                {
+                    WheelDriveID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WheelDrives", x => x.WheelDriveID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AirConditionings");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.DropTable(
+                name: "Colours");
+
+            migrationBuilder.DropTable(
+                name: "CurrentPrices");
+
+            migrationBuilder.DropTable(
+                name: "EngineTypes");
+
+            migrationBuilder.DropTable(
+                name: "Equipment");
+
+            migrationBuilder.DropTable(
+                name: "ExactTypes");
+
+            migrationBuilder.DropTable(
+                name: "GearBoxes");
+
+            migrationBuilder.DropTable(
+                name: "GeneralTypes");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "Pages");
+
+            migrationBuilder.DropTable(
+                name: "Pictures");
+
+            migrationBuilder.DropTable(
+                name: "RentalAgencies");
+
+            migrationBuilder.DropTable(
+                name: "RentalAgencyAddresses");
+
+            migrationBuilder.DropTable(
+                name: "VehicleModels");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "WheelDrives");
+        }
+    }
+}
