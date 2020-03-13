193c9e5d895f1885dc5adab9199e258ec62c1adf
fkrzysztof <krzysztofranczyk@gmail.com>
2020-03-13 12:53:27 +0100

    Przed zmianami w Identity

---
+++ Rental.Data/Data/Rental/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Rental.Data.Data.Rental
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+    }
+}
