3ba80c70cfa72e63cf9e536e30fabef73cf754ef
fkrzysztof <krzysztofranczyk@gmail.com>
2020-04-08 00:43:28 +0200

    Edycja user role - password z poz Admin

--- Rental-PI-KF/Controllers/RoleController.cs
+++ Rental-PI-KF/Controllers/RoleController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LazZiya.ImageResize;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Rental.Data;
 using Rental.Data.Data.Areas.Identity.Data;
 using Rental_PI_KF.Controllers.Abstract;
@@ -13,12 +18,12 @@ namespace Rental_PI_KF.Controllers
 
     public class RoleController : BasicControllerAbstract
     {
-      //  RoleManager<IdentityRole> _roleManager;
+        RoleManager<IdentityRole> _roleManager;
 
         public RoleController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         : base(context, userManager)
         {
-     //       _roleManager = roleManager;
+            _roleManager = roleManager;
         }
         public async Task<IActionResult> Index()
         {
@@ -43,14 +48,39 @@ namespace Rental_PI_KF.Controllers
 
         public IActionResult Administrators()
         {
-            return View();
+            List<ApplicationUser> users = _userManager.GetUsersInRoleAsync("Administrator").Result.ToList();
+            return View(users);
         }
 
         public IActionResult Employees()
         {
-            return View();
+            List<ApplicationUser> users = _userManager.GetUsersInRoleAsync("Pracownik").Result.ToList();
+            return View(users);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string id,Password pw)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if(user != null)
+            {
+                string resetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var rezult = await _userManager.ResetPasswordAsync(user, resetToken, pw.PasswordNew);
+                if(rezult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in rezult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(user);
+            
         }
 
+        
         public async Task<IActionResult> Edit(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
@@ -58,18 +88,93 @@ namespace Rental_PI_KF.Controllers
             {
                 return View("NotFound");
             }
+            var usersRole = _roleManager.Roles.ToList();
+            Password pw = new Password();
+            ViewBag.Password = pw;
+            ViewBag.UsersRole = new SelectList(usersRole, "Name", "Name");
 
             return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, ApplicationUser au, IFormFile file)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                if(file != null)
+                { 
+                    int w = 100;
+                    int h = 100;
+
+                    var stream = new MemoryStream();
+                    await file.CopyToAsync(stream);
+                    var image = Image.FromStream(stream);
+                    Image imageR = ImageResize.ScaleAndCrop(image, w, h);
+                    var streamToReturn = new MemoryStream();
+                    imageR.Save(streamToReturn, image.RawFormat);
+
+                    user.Image = streamToReturn.ToArray();
+                }
+
+                user.FirstName = au.FirstName;
+                user.LastName = au.LastName;
+                user.Email = au.Email;
+                user.Phone = au.Phone;
+                user.Street = au.Street;
+                user.Number = au.Number;
+                user.Country = au.Country;
+                user.City = au.City;
+                user.ZIPCode = au.ZIPCode;
+
+              var rezult =  await _userManager.UpdateAsync(user);
+
+                if(rezult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Edit");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(string id, string roleName)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+               var remove = _userManager.RemoveFromRoleAsync(user, _userManager.GetRolesAsync(user).Result.First());
+               var add = _userManager.AddToRoleAsync(user, roleName);
+                if (remove.Result.Succeeded && add.Result.Succeeded)
+                {
+                    return View("Index");
+                }
+            }
+            return Content("Coś poszło nie tak..");
         }
 
         public IActionResult Create()
         {
             return View(new IdentityRole());
         }
+        
         [HttpPost]
         public async Task<IActionResult> Create(IdentityRole role)
         {
       //      await _roleManager.CreateAsync(role);
+            
+        //    _userManager.
+            
             return RedirectToAction("Index");
         }
     }
---
+++ Rental.Data/Data/Areas/Identity/Data/Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Rental.Data.Data.Areas.Identity.Data
+{
+    public class Password
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} ble ble ble {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string PasswordNew { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("PasswordNew", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
